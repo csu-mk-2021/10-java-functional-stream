8acbf53bb7b01d96277af10c6f6ef5254ce5f744
blackrose198 <grushinskayan@gmail.com>
2021-11-21 21:40:53 +0500

    task02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -6,18 +6,25 @@ public class Task02Main {
 
     public static void main(String[] args) {
 
-        /*
         cycleGrayCode(2)
                 .limit(10)
                 .forEach(System.out::println);
-        */
 
     }
 
     public static IntStream cycleGrayCode(int n) {
 
-        return null; // your implementation here
-
+        if (n <1 || n > 16)
+            throw new IllegalArgumentException();
+        int count = (int) Math.pow(2, n);
+        return IntStream.iterate(0, x -> x + 1)
+                .map(g -> getGrayCode(g % count));
     }
+
+    public static int getGrayCode(int g)
+    {
+        return g ^ (g >> 1);
+    }
+
 
 }
