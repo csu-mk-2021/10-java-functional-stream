2326708b253d81457a305e2184684613124935fb
blackrose198 <grushinskayan@gmail.com>
2021-11-21 21:59:55 +0500

    task04

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,11 +1,30 @@
 package com.example.task04;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+import java.util.Comparator;
+import java.util.Map;
+
+
 public class Task04Main {
 
     public static void main(String[] args) {
 
-        // your implementation here
-
+        new BufferedReader(new InputStreamReader(System.in))
+                .lines()
+                .flatMap(x -> Stream.of(x.split("[^a-zA-Zа-яА-Я0-9ёЁ]+"))
+                        .filter(w -> !w.isEmpty())
+                        .map(String::toLowerCase))
+                .collect(Collectors.groupingBy(w -> w, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByKey())
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .limit(10)
+                .map(Map.Entry::getKey)
+                .forEach(w -> System.out.print(w + "\n"));
     }
 
 }
