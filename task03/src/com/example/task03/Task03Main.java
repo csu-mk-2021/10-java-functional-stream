36933a030fce84639ff35efdbf75de37328ca37b
danil <danilmurmya@gmail.com>
2021-11-30 22:31:41 +0500

    task03 done.

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -3,6 +3,7 @@ package com.example.task03;
 import java.util.Comparator;
 import java.util.function.BiConsumer;
 import java.util.stream.Stream;
+import java.util.Iterator;
 
 public class Task03Main {
 
@@ -20,8 +21,20 @@ public class Task03Main {
     public static <T> void findMinMax(
             Stream<? extends T> stream,
             Comparator<? super T> order,
-            BiConsumer<? super T, ? super T> minMaxConsumer) {
+            BiConsumer<? super T, ? super T> MinMaxConsumer) {
 
-        // your implementation here
+        Iterator<? extends T> iterator = stream.iterator();
+        T current = iterator.hasNext() ? iterator.next() : null;
+        T minimal = current;
+        T maximal = current;
+        while (iterator.hasNext()){
+            current = iterator.next();
+            if(order.compare(current,minimal) < 0)
+                minimal = current;
+            else if(order.compare(current,maximal) > 0)
+                maximal = current;
+        }
+
+        MinMaxConsumer.accept(minimal,maximal);
     }
 }
