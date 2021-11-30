cbc5cc5d6e58933b4cfb4522eb8cb8e7cd7cf292
danil <danilmurmya@gmail.com>
2021-11-30 22:37:03 +0500

    task04 done.

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,10 +1,25 @@
 package com.example.task04;
 
+import java.io.*;
+import java.util.stream.*;
+import java.util.*;
+
 public class Task04Main {
 
     public static void main(String[] args) {
-
-        // your implementation here
+        new BufferedReader(new InputStreamReader(System.in))
+                .lines()
+                .flatMap(x -> Stream.of(x.split("[^a-zA-Zа-яА-Я0-9ёЁ]+"))
+                        .filter(w -> !w.isEmpty())
+                        .map(String::toLowerCase))
+                .collect(Collectors.groupingBy(w -> w, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByKey())
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .limit(10)
+                .map(Map.Entry::getKey)
+                .forEach(w -> System.out.print(w + "\n"));
 
     }
 
